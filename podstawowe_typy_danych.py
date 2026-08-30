3e216451049dcf68ab817acd6526c16c7b42a239
WiktorM413 <wiktor.markowski81@gmail.com>
2026-08-30 13:09:30 +0200

    commit

---
+++ podstawowe_typy_danych.py
@@ -0,0 +1,13 @@
+# x = 5 # int
+# y = 3.14 # float
+# s = "text" # str
+
+# int() <= funkcja ktora zamienia cokolwiek na liczbe
+# "3212314" => 3212314
+
+x = int(input())
+
+
+if x == 5:
+	print("chujek")
+	print(5, "+", 0, "=", x)
---
+++ warunky.py
@@ -0,0 +1,21 @@
+# ==       sprawdza czy jest równe
+# !=       sprawdza czu jest różne
+# x > y    sprawdza czy x jest wiekszy od y
+# x < y
+# x >= y   sprawfza czy x jest wiekszy lub równy od y
+# x <= y
+
+x = 15
+
+if x == 15:
+	print("tak")
+	print("tak x 2")
+	print("tak")
+else:
+	print('nie')
+	print('nie')
+
+
+	
+print("cos")
+print("cos x 2")
